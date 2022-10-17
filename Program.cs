29731a4a3711063822dc5d5ca2477ba76250b6bc
Faust3000 <mephistopheles3000@gmail.com>
2022-10-17 19:30:32 +0400

    сделал задачу 3

--- Program.cs
+++ Program.cs
@@ -38,3 +38,18 @@ int GetSum(int number){
     }
     return sum;
 } */
+
+/* Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33] */
+
+int[] array = GetBinnaryArray(8);
+Console.WriteLine($"[{String.Join(",", array)}]");
+
+int[] GetBinnaryArray(int size){
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++){
+        result[i] = new Random().Next();
+    }
+    return result;
+}
